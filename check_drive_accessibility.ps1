4fc554534979c5c956becf6aca80bf23924e33b4
Roger <roger@sikorski.cc>
2017-03-08 10:25:54 +0100

    Update check_drive_accessibility.ps1

--- check_drive_accessibility.ps1
+++ check_drive_accessibility.ps1
@@ -6,12 +6,15 @@ Get-PSDrive $drive -EA SilentlyContinue | out-null
 
 if ($drive -eq "") {
     Write-Host "No Drive Letter as Argument!"
+    exit 2
     }
 
 elseif ($? -eq "TRUE") {
     Write-Host "$drive drive Accessible"
+    exit 0
     }
 
 else {
     Write-Host "$drive drive NOT Not Accessible / Problems"
+    exit 2
     }
