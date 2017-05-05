d9f789f57c5b151d654e786e019b4cb24fcc4d8a
Roger <roger@sikorski.cc>
2017-05-05 12:31:48 +0200

    check_files_age.ps1 - Exclusion Filter added

--- check_files_age.ps1
+++ check_files_age.ps1
@@ -1,9 +1,11 @@
 Param(
   [string]$Path,
+  [string]$ExclusionFilter1,
+  [string]$ExclusionFilter2,
   [string]$critical
 )
 
-$Status=Get-ChildItem $Path | where-object {(new-timespan $_.LastWriteTime).minutes -gt $critical}
+$Status=Get-ChildItem $Path | where-object {(new-timespan $_.LastWriteTime).minutes -gt $critical} | where { $ExclusionFilter1 -notcontains $_ } | where { $ExclusionFilter2 -notcontains $_ }
 
 
 if (!$Status) {
@@ -13,5 +15,6 @@ if (!$Status) {
 
 elseif ($Status) {
     Write-Host "Critical - Files older then $critical minutes found"
-    exit 1
+    $Status | Select Name, LastWriteTime
+    exit 2
 }
