77442479526d786f301bb8e37657b0f9021e7cb1
Roger <roger@sikorski.cc>
2017-05-08 13:51:56 +0200

    check_files_age.ps1 - Statistics Output Added

--- check_files_age.ps1
+++ check_files_age.ps1
@@ -6,15 +6,16 @@ Param(
 )
 
 $Status=Get-ChildItem $Path | where-object {(new-timespan $_.LastWriteTime).minutes -gt $critical} | where { $ExclusionFilter1 -notcontains $_ } | where { $ExclusionFilter2 -notcontains $_ }
+$StatusNumber=$Status.Count
 
 
 if (!$Status) {
-    Write-Host "OK - No files older than $critical minutes found"
+    Write-Host "OK - No files older than $critical minutes found|result=$StatusNumber;"
     exit 0
 }
 
 elseif ($Status) {
-    Write-Host "Critical - Files older than $critical minutes found"
+    Write-Host "Critical - Files older than $critical minutes found|result=$StatusNumber;"
     $Status | Select Name, LastWriteTime
     exit 2
 }
